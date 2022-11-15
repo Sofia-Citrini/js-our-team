93d6c66070d99f497ec6a82168663d1ca06d5bf2
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-15 18:47:49 +0100

    Milestone1

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Menager',
+        foto: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+for (let i = 0; 1 < team.length; i++) {
+    const membriTeam = team[i];
+    console.log(`Nome:${membriTeam.nome}, Ruolo:${membriTeam.ruolo}, Foto:${membriTeam.foto}`);
+}
